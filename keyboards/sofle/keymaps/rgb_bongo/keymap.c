c86e35ffba5816d8d13780a79b015edde71b43be
James <jamesjpeng@gmail.com>
2023-03-20 10:50:38 -0700

    swapped numpad and enter

--- keyboards/sofle/keymaps/rgb_bongo/keymap.c
+++ keyboards/sofle/keymaps/rgb_bongo/keymap.c
@@ -108,7 +108,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //|------+-------+--------+--------+--------+------|  ===  |   |  ===  |--------+-------+--------+--------+--------+---------|
   KC_LCTL,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B,  KC_MUTE,   KC_MPLY,  KC_N,    KC_M,   KC_COMM, KC_DOT,  KC_SLSH, KC_LSFT,
   //|------+-------+--------+--------+--------+------|  ===  |   |  ===  |--------+-------+--------+--------+--------+---------|
-                 MO(_ALT), KC_LALT, KC_LGUI, KC_SPC,  KC_BSPC  , KC_ENT, MO(_NUMPAD) , KC_CAPS , MO(_ADJUST), KC_LCTL
+                 MO(_ALT), KC_LALT, KC_LGUI, KC_SPC,  KC_BSPC  , MO(_NUMPAD), KC_ENT,  KC_CAPS , MO(_ADJUST), KC_LCTL
   //            \--------+--------+--------+---------+-------|   |--------+---------+--------+---------+-------/
 ),
 
@@ -144,7 +144,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_ALT] = LAYOUT(
   //,------------------------------------------------.                    ,---------------------------------------------------.
-  KC_GRV,  KC_F1,  KC_F2,   KC_F3,   KC_F4,   KC_F5,                     KC_F6,   KC_F7,   KC_F8,   KC_F9,  KC_F10,  KC_F11,
+  KC_ESC,  KC_F1,  KC_F2,   KC_F3,   KC_F4,   KC_F5,                     KC_F6,   KC_F7,   KC_F8,   KC_F9,  KC_F10,  KC_F11,
   //|------+-------+--------+--------+--------+------|                   |--------+-------+--------+--------+--------+---------|
   KC_GRV,   KC_1,   KC_2,    KC_3,    KC_UP,    KC_5,                      KC_6,    KC_7,    KC_8,    KC_9,    KC_0,   KC_BSPC,
   //|------+-------+--------+--------+--------+------|                   |--------+-------+--------+--------+--------+---------|
