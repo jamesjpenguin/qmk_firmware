e4260b3b3c56bee197dffe14d4bb9844e53b461e
James Peng <james_peng@apple.com>
2023-03-28 12:51:55 -0700

    another commit before messing with lighting

--- keyboards/sofle/keymaps/rgb_bongo/keymap.c
+++ keyboards/sofle/keymaps/rgb_bongo/keymap.c
@@ -27,40 +27,40 @@
 // Light combinations
 #define SET_INDICATORS(hsv) \
 	{0, 1, HSV_OVERRIDE_HELP(hsv, INDICATOR_BRIGHTNESS)}, \
-    {35+0, 1, hsv}
+    {36+1, 1, HSV_OVERRIDE_HELP(hsv, INDICATOR_BRIGHTNESS)}
 #define SET_UNDERGLOW(hsv) \
 	{1, 6, hsv}, \
-    {35+1, 6,hsv}
+    {36+1, 6,hsv}
 #define SET_NUMPAD(hsv)     \
 	{35+15, 5, hsv},\
-	  {35+22, 3, hsv},\
-	  {35+27, 3, hsv}
+	  {36+22, 3, hsv},\
+	  {36+27, 3, hsv}
 #define SET_NUMROW(hsv) \
 	{10, 2, hsv}, \
 		{20, 2, hsv}, \
 		{30, 2, hsv}, \
-	  {35+ 10, 2, hsv}, \
-	  {35+ 20, 2, hsv}, \
-	  {35+ 30, 2, hsv}
+	  {36+ 10, 2, hsv}, \
+	  {36+ 20, 2, hsv}, \
+	  {36+ 30, 2, hsv}
 #define SET_INNER_COL(hsv)	\
 	{33, 4, hsv}, \
-	  {35+ 33, 4, hsv}
+	  {36+ 33, 4, hsv}
 
 #define SET_OUTER_COL(hsv) \
 	{7, 4, hsv}, \
-	  {35+ 7, 4, hsv}
+	  {36+ 7, 4, hsv}
 #define SET_THUMB_CLUSTER(hsv) 	\
 	{25, 2, hsv}, \
-	  {35+ 25, 2, hsv}
+	  {36+ 25, 2, hsv}
 #define SET_LAYER_ID(hsv) 	\
 	{0, 1, HSV_OVERRIDE_HELP(hsv, INDICATOR_BRIGHTNESS)}, \
-    {35+0, 1, HSV_OVERRIDE_HELP(hsv, INDICATOR_BRIGHTNESS)}, \
+    {36+0, 1, HSV_OVERRIDE_HELP(hsv, INDICATOR_BRIGHTNESS)}, \
 		{1, 6, hsv}, \
-    {35+1, 6, hsv}, \
+    {65+1, 6, hsv}, \
 		{7, 4, hsv}, \
-	  {35+ 7, 4, hsv}, \
+	  {36+ 7, 4, hsv}, \
 		{25, 2, hsv}, \
-	  {35+ 25, 2, hsv}
+	  {36+ 25, 2, hsv}
 
 char wpm_str[10];
 
